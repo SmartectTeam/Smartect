9af4e139f6482082836091df8e5a49546efb814e
320-16\hsy <duftkd0804@gmail.com>
2025-12-11 14:42:21 +0900

    camera 설정 확대 모두 사용 가능

--- WorkSpace_Python/apps/action_router/main.py
+++ WorkSpace_Python/apps/action_router/main.py
@@ -4,7 +4,10 @@
 
 from endpoints import camera_post_video
 import asyncio
+import os
 
+pc_id = "HSYPC"
+cam_id = 0
 
 if __name__ == "__main__":
-    asyncio.run(camera_post_video())
+    asyncio.run(camera_post_video(pc_id, cam_id))
