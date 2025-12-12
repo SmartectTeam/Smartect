0680a66fd1af84e141d1c690018790a26c6e94c8
320-25\chlwngur <choljoohuck@gmail.com>
2025-12-12 17:23:19 +0900

    초기

---
+++ Controller_Java/src/main/java/org/smartect/controller/StatisticsController.java
@@ -0,0 +1,13 @@
+package org.smartect.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class StatisticsController {
+
+    @GetMapping("/statistics")
+    public String statistics() {
+        return "statistics";
+    }
+}
