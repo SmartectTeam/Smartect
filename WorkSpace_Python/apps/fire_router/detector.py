7ccda80383ec0133ba89f2fc365b8fd2a0de7164
320-16\hsy <duftkd0804@gmail.com>
2025-12-23 18:19:49 +0900

    25.12.23

--- WorkSpace_Python/apps/fire_router/detector.py
+++ WorkSpace_Python/apps/fire_router/detector.py
@@ -54,7 +54,7 @@ def fire_model_video(model, frame, threshold_map, cam_no):
                         event_type="fire",
                         danger_level=3,
                         event_time=datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-                        screenshot_path="/images/fire_detected.jpg"
+                        screenshot_path=""
                     ))
 
                 elif class_name == "smoke":
@@ -63,7 +63,7 @@ def fire_model_video(model, frame, threshold_map, cam_no):
                         event_type="smoke",
                         danger_level=3,
                         event_time=datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-                        screenshot_path="/images/fire_detected.jpg"
+                        screenshot_path=""
                     ))
 
     return fire_json, fire_map
