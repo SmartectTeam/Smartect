277339f719ff5134f372dcdc669349f84825eee9
320-16\hsy <duftkd0804@gmail.com>
2025-12-24 11:57:17 +0900

    v5

--- Controller_Java/src/main/java/org/smartect/service/EventJsonService.java
+++ Controller_Java/src/main/java/org/smartect/service/EventJsonService.java
@@ -1,4 +1,3 @@
-
 package org.smartect.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -96,12 +95,10 @@ public class EventJsonService {
     private void processEvent(EventJsonDTO event, EventMapDTO map, byte[] Img_bytes) {
         String eventType = event.getEvent_type();
 
-// 필터링
         if (!isAllowedEventType(eventType)) {
             return;
         }
 
-// 쿨다운
         if (!shouldSaveEvent(event.getCam_no(), eventType)) {
             return;
         }
